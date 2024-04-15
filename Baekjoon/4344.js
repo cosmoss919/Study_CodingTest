521425a0ff04855272f7e4e5c233385e6188c989
Park Na Jin <162278527+cosmoss919@users.noreply.github.com>
2024-04-15 18:08:56 +0900

    백준 문제풀이

---
+++ Baekjoon/4344.js
@@ -0,0 +1,13 @@
+//https://www.acmicpc.net/problem/4344
+
+const fs = require('fs');
+const [count, ...input] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+input.forEach(t => {
+    const score = t.split(' ').slice(1).map(Number);
+    const count = score.length;
+    const avg = score.reduce((a,b) => a + b) / count;
+    console.log(`${((score.filter(s => s > avg).length / count) * 100).toFixed(3)}%`);
+})
+
+

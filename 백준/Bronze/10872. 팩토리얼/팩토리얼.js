62d5356c568ea6ee893453d63d9a387c1f74e6c1
Park Na Jin <162278527+cosmoss919@users.noreply.github.com>
2024-05-23 13:55:00 +0900

    [Bronze III] Title: 팩토리얼, Time: 120 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const num = fs.readFileSync('/dev/stdin').toString();
+console.log(pactorial(num));
+function pactorial(num) {
+    if(num <= 1) {
+        return 1;
+    } else {
+        return num * pactorial(num - 1);
+    }
+}

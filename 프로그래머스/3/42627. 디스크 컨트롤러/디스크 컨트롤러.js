49bdd039c1df1067dafc59630198721fea3313d3
Park Na Jin <162278527+cosmoss919@users.noreply.github.com>
2024-10-28 23:03:06 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -1,25 +1,29 @@
 function solution(jobs) {
-    jobs.sort((a, b) => a[0] - b[0]); // 도착 시간 기준으로 정렬
-    const heap = [];
-    let time = 0;
-    let index = 0;
-    let answer = 0;
-
-    while (index < jobs.length || heap.length > 0) {
-        // 현재 시간 이하로 도착한 작업들을 힙에 추가
-        if (index < jobs.length && jobs[index][0] <= time) {
-            heap.push(jobs[index++]);
-            heap.sort((a, b) => a[1] - b[1]); // 작업 시간을 기준으로 정렬
-            continue; // 작업이 추가되면 다음 while 반복으로 이동
+    let answer = jobs[0][1];
+    jobs = jobs.sort((a, b) => a[0] - b[0]);
+    let time = jobs[0][1];
+    // 첫번째꺼 이후로 작업 시간이 짧은 순으로 정렬
+    let sliceJobs = jobs.slice(1).sort((a, b) => a[1] - b[1] || a[0] - b[0]);
+    let i = 0;
+    while (sliceJobs.length > 0) {
+        if(sliceJobs[i][0] <= time) {
+            time += sliceJobs[i][1];
+            answer += time - sliceJobs[i][0];
+            sliceJobs.splice(i, 1);
+            i = 0;
+        } else {
+            i++;
         }
 
-        // 힙에 작업이 있으면 작업을 처리하고, 없으면 다음 작업 시간으로 이동
-        if (heap.length > 0) {
-            const [start, duration] = heap.shift();
-            time += duration;
-            answer += time - start;
-        } else {
-            time = jobs[index][0];
+        if(i === sliceJobs.length) {
+            let findIndex = sliceJobs.findIndex(j => j[0] === Math.min(...sliceJobs.map(s => s[0])))
+            if(findIndex === -1) {
+                break;
+            }
+            time = sliceJobs[findIndex][0] + sliceJobs[findIndex][1];
+            answer += sliceJobs[findIndex][1];
+            sliceJobs.splice(findIndex, 1);
+            i = 0;
         }
     }
 

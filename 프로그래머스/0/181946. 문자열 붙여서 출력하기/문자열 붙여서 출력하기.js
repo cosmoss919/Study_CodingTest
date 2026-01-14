588d4212d3ac3e5902afc8dc209587db3e3a8e9b
Park Na Jin <162278527+cosmoss919@users.noreply.github.com>
2026-01-14 15:26:43 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 60.22 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.js
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.js
@@ -5,13 +5,7 @@ const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
-
-let input = [];
-
-rl.on('line', function (line) {
-    input = line.split(' ');
-}).on('close', function () {
-    let [a, b] = input;
-    console.log(`${a}${b}`);
-});
+}).on('line', function (line) {
+    const strArr = line.split(' ')
+    console.log(strArr.join(''))
+})

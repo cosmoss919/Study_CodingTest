fef5c284e45779789f439cb123546ef7da5fc4e7
Park Na Jin <162278527+cosmoss919@users.noreply.github.com>
2026-01-16 15:50:16 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -7,3 +7,25 @@ function solution(a, b, c) {
         return (a + b + c) * (a*a + b*b + c*c );
    }
 }
+
+
+/*
+const solution = (a, b, c) => {
+    const set = new Set([a, b, c]);
+    switch ([...set].length) {
+        case 1: return calculate([a, b, c], 3);
+        case 2: return calculate([a, b, c], 2);
+        case 3: return calculate([a, b, c]);
+    }
+};
+
+const calculate = (inc, n=1) => {
+    const [a, b, c] = inc;
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= Math.pow(a, i) + Math.pow(b, i) + Math.pow(c, i)
+    }
+
+    return result;
+};
+*/

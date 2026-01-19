fc3b333b353925e1b519d8e74d2ff80a9a692063
Park Na Jin <162278527+cosmoss919@users.noreply.github.com>
2026-01-19 15:07:58 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.09 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -0,0 +1,9 @@
+//정수가 담긴 리스트 num_list가 주어질 때, 모든 원소들의 곱이 모든 원소들의 합의 제곱보다 작으면 1을 크면 0을 return하도록 solution 함수를 완성해주세요.
+
+
+function solution(num_list) {
+    let a = num_list.reduce((acc, num) => acc * num, 1);
+    let b = Math.pow(num_list.reduce((acc, num) => acc + num, 0), 2);
+
+    return +(a < b);
+}

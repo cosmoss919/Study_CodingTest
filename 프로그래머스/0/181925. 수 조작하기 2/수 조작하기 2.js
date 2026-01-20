54b85c24f525001ea9a83df4f1ba5e96960c629c
Park Na Jin <162278527+cosmoss919@users.noreply.github.com>
2026-01-20 13:57:45 +0900

    [level 0] Title: 수 조작하기 2, Time: 10.37 ms, Memory: 45.5 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -7,3 +7,11 @@ function solution(numLog) {
      }
      return result;
 }
+
+/*
+function solution(numLog) {
+    const map = { 1: 'w', -1: 's', 10: 'd', -10: 'a' };
+    return numLog.slice(1).map((v, i) => map[v - numLog[i]]).join('');
+}
+
+*/

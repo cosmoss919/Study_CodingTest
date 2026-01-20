5425927ff723bb00d0efe91f6f91e0a1d63e25d2
Park Na Jin <162278527+cosmoss919@users.noreply.github.com>
2026-01-20 14:08:49 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.32 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -9,3 +9,13 @@ function solution(arr, queries) {
 
     return arr;
 }
+
+/*
+function solution(arr, queries) {
+    for (const [i, j] of queries) {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
+
+*/

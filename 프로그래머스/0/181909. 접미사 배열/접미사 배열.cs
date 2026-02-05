8b0e1c1e802546ee1e39a76739da575fb4796be8
Park Na Jin <162278527+cosmoss919@users.noreply.github.com>
2026-02-05 16:28:07 +0900

    [level 0] Title: 접미사 배열, Time: 10.07 ms, Memory: 31.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public string[] solution(string my_string) {
+        string[] answer = new string[my_string.Length];
+
+        for (int i = 0; i < my_string.Length; i++)
+        {
+            answer[i] = my_string.Substring(i);
+        }
+
+        return answer.OrderBy(t => t).ToArray();
+    }
+}

43badd12c9c9a2e4dea1c01cd99a787e90dd6771
Park Na Jin <162278527+cosmoss919@users.noreply.github.com>
2026-02-05 16:37:49 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.60 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cs
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cs
@@ -13,3 +13,15 @@ public class Solution {
         return stirngArr.Contains(is_suffix) ? 1 : 0;
     }
 }
+
+/*
+using System;
+
+public class Solution {
+    public int solution(string my_string, string is_suffix)
+    {
+        return my_string.EndsWith(is_suffix) ? 1 : 0;
+    }
+}
+
+*/

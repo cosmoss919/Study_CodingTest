a430bd62e8ac8331a744e656a036022a24c44daf
Park Na Jin <162278527+cosmoss919@users.noreply.github.com>
2026-02-11 17:48:46 +0900

    [level 2] Title: 기능개발, Time: 2.30 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cs
+++ 프로그래머스/2/42586. 기능개발/기능개발.cs
@@ -32,3 +32,41 @@ public class Solution {
         return answer.ToArray();
     }
 }
+
+/*
+using System;
+using System.Collections.Generic;
+
+public class Solution 
+{
+    public int[] solution(int[] progresses, int[] speeds)
+    {
+        var answer = new List<int>();
+        var queue = new Queue<int>();
+
+        // 1. 완료까지 걸리는 날짜 계산해서 큐에 저장
+        for (int i = 0; i < progresses.Length; i++)
+        {
+            int day = (int)Math.Ceiling((100.0 - progresses[i]) / speeds[i]);
+            queue.Enqueue(day);
+        }
+
+        // 2. 배포 묶기
+        while (queue.Count > 0)
+        {
+            int current = queue.Dequeue();
+            int count = 1;
+
+            while (queue.Count > 0 && queue.Peek() <= current)
+            {
+                queue.Dequeue();
+                count++;
+            }
+
+            answer.Add(count);
+        }
+
+        return answer.ToArray();
+    }
+}
+*/

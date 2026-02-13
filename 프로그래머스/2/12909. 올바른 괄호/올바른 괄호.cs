91f5683d09cc639102cd3454436ff6bcffeb034a
Park Na Jin <162278527+cosmoss919@users.noreply.github.com>
2026-02-13 11:24:21 +0900

    [level 2] Title: 올바른 괄호, Time: 1.78 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cs
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cs
@@ -20,3 +20,50 @@ public class Solution {
         return stack.Count == 0;
     }
 }
+
+/*
+올바른 풀이
+public bool solution(string s)
+{
+    var stack = new Stack<char>();
+
+    foreach (var c in s)
+    {
+        if (c == '(')
+        {
+            stack.Push(c);
+        }
+        else // ')'
+        {
+            if (stack.Count == 0)
+                return false;
+
+            stack.Pop();
+        }
+    }
+
+    return stack.Count == 0;
+}
+
+스택없이 풀이
+public bool solution(string s)
+{
+    int count = 0;
+
+    foreach (var c in s)
+    {
+        if (c == '(')
+            count++;
+        else
+        {
+            count--;
+            if (count < 0)
+                return false;
+        }
+    }
+
+    return count == 0;
+}
+
+
+*/
